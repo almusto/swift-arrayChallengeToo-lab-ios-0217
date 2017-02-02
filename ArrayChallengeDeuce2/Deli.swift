7442b8730bf6ab66d1cbc33f89c9e7f288e72130
almusto <ales.musto@gmail.com>
2017-02-01 22:47:46 -0500

    deli2.0

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -17,12 +17,37 @@ class Deli {
     func addNameToLine(name: String) -> String {
                 
        // TODO: Implement this function.
+
+      if name == "Billy Crystal" || name == "Meg Ryan" {
+        line.insert(name, at: 0)
+      } else {
+        line.append(name)
+      }
+
+      let index = line.index(of: name)
+      if line[index!] == "Billy Crystal" {
+        return "Welcome Billy Crystal! You can sit wherever you like."
+      } else if line[index!] == "Meg Ryan" {
+        return "Welcome Meg Ryan! You can sit wherever you like."
+      } else if index == 0 {
+        return "Welcome \(name), you're first in line!"
+      } else {
+        return "Welcome \(name), you're number \(index!+1) in line."
+      }
+      
         
     }
     
     
     // 2
     func nowServing() -> String {
+      if line.isEmpty {
+        return "There is no one to be served."
+      } else {
+        let name = line[0]
+        line.remove(at: 0)
+        return "Now serving \(name)!"
+      }
         
         // TODO: Implement this function.
 
@@ -31,7 +56,17 @@ class Deli {
     
     // 3
     func lineDescription() -> String {
-        
+      var string = "The line is:"
+      if line.isEmpty {
+        return "The line is currently empty."
+      } else {
+
+        for (index, name) in line.enumerated() {
+           string.append("\n\(index+1). \(name)")
+        }
+        return string
+      }
+
         // TODO: Implement this function.
 
     }
